202e7188a6768c8934ee5b9dfc749c900141a98c
Kevin Ostafinski <kevinostafinski@gmail.com>
2018-11-05 20:54:56 +0100

    wrapped into flex container, added main background

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ sideFooterWrap = document.querySelector('.sidebar__footer__wrapper');
 sideSquareWrap = document.querySelector('.sidebar__square__wrapper');
 sideMainWrap = document.querySelector('.sidebar__main__wrapper');
 
+mainBgWrap = document.querySelector('.main__bg__wrapper');
+
 function onWheelScroll() {
   sideFooterWrap.style.transition = 'all 0.8s cubic-bezier(.64,1.05,.89,.95)'
   sideSquareWrap.style.transition = 'all 0.8s cubic-bezier(.64,1.05,.89,.95)'
   sideMainWrap.style.transition = 'all 0.8s cubic-bezier(.64,1.05,.89,.95)'
+  mainBgWrap.style.transition = 'all 0.8s cubic-bezier(.64,1.05,.89,.95)'
 
   console.log(window.getComputedStyle(sideFooterWrap).getPropertyValue('transform'))
 
@@ -13,12 +16,14 @@ function onWheelScroll() {
     || window.getComputedStyle(sideFooterWrap).getPropertyValue('transform') == 'none') {
     sideFooterWrap.style.transform = 'translateX(-50%)'
     sideSquareWrap.style.transform = 'translateX(0)'
-    sideMainWrap.style.transform = 'translateX(-0)'
+    sideMainWrap.style.transform = 'translateX(0)'
+    mainBgWrap.style.transform = 'translateX(0)'
 
   } else {
     sideFooterWrap.style.transform = 'translateX(0)'
     sideSquareWrap.style.transform = 'translateX(-50%)'
     sideMainWrap.style.transform = 'translateX(-50%)'
+    mainBgWrap.style.transform = 'translateX(-50%)'
   }
 }
 
